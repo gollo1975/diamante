3dd8f184ccf3188fa78b5d2f07ec92313a45bd46
gollo1975 <jgpmorales1975@hotmail.com>
2024-06-27 17:36:45 -0500

    libreria

--- config/db.php
+++ config/db.php
@@ -3,9 +3,10 @@
 return [
    'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=erpdiamante',
-    'username' => 'root',
-    'password' => '',
+    'username' => 'desarrollo',
+    'password' => 'm@quil@1119',
     'charset' => 'utf8',
+    'attributes' => [PDO::ATTR_CASE => PDO::CASE_LOWER],
     
    /* 'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=192.168.1.2;dbname=erpdiamante',
