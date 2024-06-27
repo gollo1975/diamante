89a40ae021a7d767b60b2e7a90e50aeba50d96ac
gollo1975 <jgpmorales1975@hotmail.com>
2024-06-27 11:57:35 -0500

    libreria

--- themes/adminLTE/clientes/_form.php
+++ themes/adminLTE/clientes/_form.php
@@ -15,7 +15,7 @@ use app\models\PosicionPrecio;
 use app\models\AgentesComerciales;
 use app\models\TipoCliente;
 
-
+//asdshdghasdhas;
 $departamento = ArrayHelper::map(Departamentos::find()->orderBy('departamento DESC')->all(), 'codigo_departamento', 'departamento');
 $municipio = ArrayHelper::map(Municipios::find()->orderBy('municipio DESC')->all(), 'codigo_municipio', 'municipio');
 $tipodocumento = ArrayHelper::map(TipoDocumento::find()->where(['=','proceso_cliente', 1])->all(), 'id_tipo_documento', 'documento');
